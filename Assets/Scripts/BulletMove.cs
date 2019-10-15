242d7b68c8f539f6687b5d3acfe0e18b06933cc0
katomasahiro10 <katomasahiro10@gmail.com>
2019-10-15 16:49:27 +0900

    時間経過で弾が消えるようにした

--- Assets/Scripts/BulletMove.cs
+++ Assets/Scripts/BulletMove.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class BulletMove : MonoBehaviour
 {
+    float max_lifespan = 1.5f;
+    float lifetime = 0.0f;
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +15,11 @@ public class BulletMove : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        lifetime += Time.deltaTime;
         transform.Translate(transform.forward * 0.5f * -1);
+        if(lifetime >= max_lifespan)
+        {
+            Destroy(this.gameObject);
+        }
     }
 }
--- Assets/Scripts/ShotBullet.cs
+++ Assets/Scripts/ShotBullet.cs
@@ -16,12 +16,8 @@ public class ShotBullet : MonoBehaviour
         GameObject obj = (GameObject)Resources.Load("Bullet");
         if (Input.GetKeyDown(KeyCode.F))
         {
-            Debug.Log(transform.rotation.eulerAngles.y);
-            //Instantiate(obj, new Vector3(0.0f, 2.0f, 0.0f), Quaternion.identity);
-            PlayerPosition = transform.position;
-            Instantiate(obj, PlayerPosition, Quaternion.Euler(90,transform.rotation.eulerAngles.y ,0));
-
+            GameObject firepoint = GameObject.Find("FirePoint");
+            Instantiate(obj, firepoint.transform.position , Quaternion.Euler(90,transform.rotation.eulerAngles.y ,0));
         }
-
     }
 }
