372e9a0c019eca3da18cbd280387f84fc0a822fe
katomasahiro10 <katomasahiro10@gmail.com>
2019-10-16 00:44:21 +0900

    Squashed commit of the following:
    弾丸発射まで
    
    commit 1177e0837ced0a61da2c42046bb1202554f83170
    Author: katomasahiro10 <katomasahiro10@gmail.com>
    Date:   Wed Oct 16 00:41:58 2019 +0900
    
        余計なもの削除
    
    commit b80b8d8d1f7cf7f42cfa6a96f9c4be8d3f4e7a22
    Author: katomasahiro10 <katomasahiro10@gmail.com>
    Date:   Tue Oct 15 17:45:53 2019 +0900
    
        metaデータ追加
    
    commit 242d7b68c8f539f6687b5d3acfe0e18b06933cc0
    Author: katomasahiro10 <katomasahiro10@gmail.com>
    Date:   Tue Oct 15 16:49:27 2019 +0900
    
        時間経過で弾が消えるようにした
    
    commit c1653d395cc07c029b3c35b3ec98d1a29f4d2803
    Author: katomasahiro10 <katomasahiro10@gmail.com>
    Date:   Tue Oct 15 15:00:22 2019 +0900
    
        弾を撃てるようになった
    
    commit ab4e9b3ed5ec3b146daa6e1eaa9fe70e01d4ffc2
    Author: katomasahiro10 <katomasahiro10@gmail.com>
    Date:   Tue Oct 15 01:08:20 2019 +0900
    
        とりあえず弾丸のプレハブを作り出せるようになった
    
        産卵かな？

---
+++ Assets/Scripts/PrefabTest.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PrefabTest : MonoBehaviour
+{
+
+    private Vector3 PlayerPosition;
+    // Use this for initialization
+    void Start()
+    {
+        // CubeプレハブをGameObject型で取得
+        GameObject obj = (GameObject)Resources.Load("Bullet");
+        // Cubeプレハブを元に、インスタンスを生成、
+        Instantiate(obj, new Vector3(0.0f, 2.0f, 0.0f), Quaternion.identity);
+    }
+
+    void Update()
+    {
+        GameObject obj = (GameObject)Resources.Load("Bullet");
+        if(Input.GetKey(KeyCode.F))
+        {
+            //Instantiate(obj, new Vector3(0.0f, 2.0f, 0.0f), Quaternion.identity);
+            Debug.Log(transform.rotation.eulerAngles.x);
+            PlayerPosition = transform.position;
+            Instantiate(obj, PlayerPosition, Quaternion.identity);
+        }
+
+    }
+
+}
---
+++ Assets/Scripts/ShotBullet.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShotBullet : MonoBehaviour
+{
+    private Vector3 PlayerPosition;
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+        GameObject obj = (GameObject)Resources.Load("Bullet");
+        if (Input.GetKeyDown(KeyCode.F))
+        {
+            GameObject firepoint = GameObject.Find("FirePoint");
+            Instantiate(obj, firepoint.transform.position , Quaternion.Euler(90,transform.rotation.eulerAngles.y ,0));
+        }
+    }
+}
