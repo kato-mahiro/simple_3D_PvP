117a933fd7e9d3ed3de83d03d66322c53b917dbb
katomasahiro10 <katomasahiro10@gmail.com>
2019-10-24 20:31:43 +0900

    スタミナゲージを管理するスクリプトを追加

---
+++ Assets/Scripts/UIDirector.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIDirector : MonoBehaviour
+{
+    GameObject staminaGauge;
+    // Start is called before the first frame update
+    void Start()
+    {
+        this.staminaGauge = GameObject.Find("staminaGauge");
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        
+    }
+
+    public void DecreaseStamina()
+    {
+        this.staminaGauge.GetComponent<Image>().fillAmount -= 0.1f;
+    }
+}
